59a6af22798c048757173a8cd63343c2fc6af33d
MS Ahsan <ahsanaan1306@gmail.com>
2023-08-02 20:23:27 +0700

    pizza-menu : video 39

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -71,6 +71,15 @@ function Menu() {
 }
 
 function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+
+  // if (hour >= openHour && hour <= closeHour)
+  //   alert('were currently open');
+  // else alert('Sorry were close');
+
   return (
     <footer>
       {' '}
