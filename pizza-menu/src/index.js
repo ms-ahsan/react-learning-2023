a91aca91c1e1e52af91bb362dacc9de87ebd4c6d
MS Ahsan <ahsanaan1306@gmail.com>
2023-08-04 00:15:53 +0700

    pizza-menu : video 51

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -102,19 +102,16 @@ function Menu() {
   );
 }
 
-function Pizza(props) {
-  if (props.pizzaObject.soldOut) return null;
+function Pizza({ pizzaObject }) {
+  if (pizzaObject.soldOut) return null;
 
   return (
     <li className='pizza'>
-      <img
-        src={props.pizzaObject.photoName}
-        alt={props.pizzaObject.name}
-      />
+      <img src={pizzaObject.photoName} alt={pizzaObject.name} />
       <div>
-        <h3>{props.pizzaObject.name}</h3>
-        <p>{props.pizzaObject.ingredients}</p>
-        <span>{props.pizzaObject.price}</span>
+        <h3>{pizzaObject.name}</h3>
+        <p>{pizzaObject.ingredients}</p>
+        <span>{pizzaObject.price}</span>
       </div>
     </li>
   );
@@ -141,7 +138,7 @@ function Footer() {
   return (
     <footer className='footer'>
       {isOpen ? (
-        <Order closeHour={closeHour} />
+        <Order closeHour={closeHour} openHour={openHour} />
       ) : (
         <p>
           We're happy to welcome you between {openHour}:00. Come visit
@@ -152,12 +149,12 @@ function Footer() {
   );
 }
 
-function Order(props) {
+function Order({ closeHour, openHour }) {
   return (
     <div className='order'>
       <p>
-        We're open until {props.closeHour}:00. Come visit us or order
-        online.
+        We're open from {openHour}:00 to {closeHour}:00. Come visit us
+        or order online.
       </p>
       <button className='btn'>Order</button>
     </div>
