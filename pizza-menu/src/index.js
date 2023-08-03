c0d3c9a2fe6ba87d18af5aedb90d443d657d638b
MS Ahsan <ahsanaan1306@gmail.com>
2023-08-04 00:03:17 +0700

    pizza-menu : video 48

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -75,17 +75,21 @@ function Header() {
 
 function Menu() {
   const pizzas = pizzaData;
-
+  const numPizzas = pizzas.length;
   return (
     <main className='menu'>
       <h2>Our menu</h2>
 
-      {pizzas && (
+      {numPizzas > 0 ? (
         <ul className='pizzas'>
           {pizzas.map((pizza) => (
             <Pizza pizzaObject={pizza} key={pizza.name} />
           ))}
         </ul>
+      ) : (
+        <p>
+          We're still working on our menu. Please come back later:){' '}
+        </p>
       )}
 
       {/* <Pizza
@@ -126,14 +130,19 @@ function Footer() {
 
   return (
     <footer className='footer'>
-      {isOpen && (
+      {isOpen ? (
         <div className='order'>
           <p>
             We're open until {closeHour}:00. Come visit us or order
-            onlone.
+            online.
           </p>
           <button className='btn'>Order</button>
         </div>
+      ) : (
+        <p>
+          We're happy to welcome you between {openHour}:00. Come visit
+          us or order online.
+        </p>
       )}
     </footer>
   );
