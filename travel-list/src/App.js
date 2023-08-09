39a35f7f639e1ea559917eac196fbc448ef619b0
Muhammad Salimy Ahsan <71083634+ms-ahsan@users.noreply.github.com>
2023-08-09 20:54:46 +0700

    travel-list : video 85

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -34,7 +34,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -124,11 +124,26 @@ function Item({ item, onDeleteItem, onToggleItem }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length) {
+    return (
+      <p className='stats'>
+        <em>Start adding some items to your packing list</em>
+      </p>
+    );
+  }
+
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
+
   return (
     <footer className='stats'>
       <em>
-        You have X item on your list, and you already packed X (X%)
+        {percentage === 100
+          ? 'You got everything! Ready to go'
+          : `You have ${numItems} item on your list, and you already packed ${' '}
+          ${numPacked} (${percentage}%)`}
       </em>
     </footer>
   );
