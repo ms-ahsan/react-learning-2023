ef7e5719e41be1def8b0923cf079aec6e3b17b70
Muhammad Salimy Ahsan <71083634+ms-ahsan@users.noreply.github.com>
2023-08-09 13:47:18 +0700

    travel-list : video 80

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -7,11 +7,16 @@ const initialItems = [
 ];
 
 export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
   return (
     <div className='app'>
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleAddItems} />
+      <PackingList items={items} />
       <Stats />
     </div>
   );
@@ -21,7 +26,7 @@ function Logo() {
   return <h1>🌴Far Away💼</h1>;
 }
 
-function Form() {
+function Form({ onAddItems }) {
   const [description, setDescription] = useState('');
   const [quantity, setQuantity] = useState(1);
 
@@ -36,6 +41,8 @@ function Form() {
       package: false,
       id: Date.now(),
     };
+
+    onAddItems(newItem);
 
     setDescription('');
     setQuantity(1);
@@ -65,11 +72,11 @@ function Form() {
   );
 }
 
-function PackingList() {
+function PackingList({ items }) {
   return (
     <div className='list'>
       <ul>
-        {initialItems.map((item) => (
+        {items.map((item) => (
           <Item item={item} key={item.id} />
         ))}
       </ul>
