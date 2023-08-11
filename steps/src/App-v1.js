2ded0321f389a241e69f5a62ec08df8cfe5f003d
MS Ahsan <ahsanaan1306@gmail.com>
2023-08-12 00:08:55 +0700

    steps : video 90

--- steps/src/App-v1.js
+++ steps/src/App-v1.js
@@ -10,7 +10,7 @@ export default function App() {
   return (
     <div>
       <Steps />
-      {/* <Steps /> */}
+      <Steps />
     </div>
   );
 }
@@ -59,36 +59,21 @@ function Steps() {
           </p>
 
           <div className='buttons'>
-            <Button
-              bgColor='#7950f2'
-              textColor='#fff'
+            <button
+              style={{ backgroundColor: '#7950f2', color: '#fff' }}
               onClick={handlePrevious}
             >
-              <span>👈</span> Previous
-            </Button>
-
-            <Button
-              bgColor='#7950f2'
-              textColor='#fff'
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: '#7950f2', color: '#fff' }}
               onClick={handleNext}
             >
-              Next <span>👉</span>
-              <span>🤓</span>
-            </Button>
+              Next
+            </button>
           </div>
         </div>
       )}
     </div>
-  );
-}
-
-function Button({ textColor, bgColor, onClick, children }) {
-  return (
-    <button
-      style={{ backgroundColor: bgColor, color: textColor }}
-      onClick={onClick}
-    >
-      {children}
-    </button>
   );
 }
