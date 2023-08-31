65d014329b5226ac52a20e358baec325437f5428
MS Ahsan <ahsanaan1306@gmail.com>
2023-08-31 11:13:10 +0700

    usepopcorn : video 142

--- usepopcorn/src/index.js
+++ usepopcorn/src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App';
+import './index.css';
+import App from './App';
 
 import StarRating from './StarRating';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating
+    <App />
+    {/* <StarRating
       maxRating={5}
       messages={['terrible', 'bad', 'okay', 'good', 'amazing']}
-    />
+    /> */}
   </React.StrictMode>
 );
