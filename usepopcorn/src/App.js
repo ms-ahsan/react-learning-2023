3054df34e0bb58c51f833d339fd7a8326113c77c
Muhammad Salimy Ahsan <71083634+ms-ahsan@users.noreply.github.com>
2023-09-07 23:30:57 +0700

    usepopcorn : video 153

--- usepopcorn/src/App.js
+++ usepopcorn/src/App.js
@@ -330,6 +330,11 @@ function MovieDetails({
     onCloseMovie();
   }
 
+  useEffect(() => {
+    if (!title) return;
+    document.title = `Movie | ${title}`;
+  }, [title]);
+
   return (
     <div className='details'>
       {isLoading ? (
