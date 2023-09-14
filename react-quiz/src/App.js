1cf7c73b4ffdf680b2a5ab5dea3a4d7b4786012d
Muhammad Salimy Ahsan <71083634+ms-ahsan@users.noreply.github.com>
2023-09-14 12:05:00 +0700

    react-quiz : video 192

--- react-quiz/src/App.js
+++ react-quiz/src/App.js
@@ -1,6 +1,9 @@
 import { useEffect, useReducer } from 'react';
 import Header from './Header';
 import Main from './Main';
+import Loader from './Loader';
+import Error from './Error';
+import StartScreen from './StartScreen';
 
 const initialState = {
   questions: [],
@@ -22,6 +25,8 @@ function reducer(state, action) {
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
 
+  const { questions, status } = state;
+  const numQuestions = questions.length;
   useEffect(() => {
     fetch('http://localhost:8000/questions')
       .then((res) => res.json())
@@ -35,8 +40,11 @@ function App() {
       <Header />
 
       <Main>
-        <p>1/15</p>
-        <p>Question?</p>
+        {status === 'loading' && <Loader />}
+        {status === 'error' && <Error />}
+        {status === 'ready' && (
+          <StartScreen numQuestions={numQuestions} />
+        )}
       </Main>
     </div>
   );
