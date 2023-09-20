5efc11e7d458d7e45005547df863a2ce5b1c37cc
Muhammad Salimy Ahsan <71083634+ms-ahsan@users.noreply.github.com>
2023-09-20 22:50:28 +0700

    worldwise : video 217

--- worldwise/src/components/City.jsx
+++ worldwise/src/components/City.jsx
@@ -1,4 +1,4 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useSearchParams } from 'react-router-dom';
 import styles from './City.module.css';
 
 const formatDate = (date) =>
@@ -11,7 +11,9 @@ const formatDate = (date) =>
 
 function City() {
   const { id } = useParams();
-
+  const [searchParams, setSearchParam] = useSearchParams();
+  const lat = searchParams.get('lat');
+  const lng = searchParams.get('lng');
   // TEMP DATA
   const currentCity = {
     cityName: 'Lisbon',
