dc1855e4e1c479a102432fc4496a3644beef52a1
Muhammad Salimy Ahsan <71083634+ms-ahsan@users.noreply.github.com>
2023-09-20 20:01:52 +0700

    worldwise : video 210

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -1,8 +1,9 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Homepage from './pages/Homepage';
-import Product from './pages/product';
-import Pricing from './pages/pricing';
+import Product from './pages/Product';
+import Pricing from './pages/Pricing';
 import PageNotFound from './pages/PageNotFound';
+import Login from './pages/Login';
 import AppLayout from './pages/AppLayout';
 
 export default function App() {
@@ -12,6 +13,7 @@ export default function App() {
         <Route path='/' element={<Homepage />} />
         <Route path='product' element={<Product />} />
         <Route path='pricing' element={<Pricing />} />
+        <Route path='login' element={<Login />} />
         <Route path='app' element={<AppLayout />} />
         <Route path='*' element={<PageNotFound />} />
       </Routes>
