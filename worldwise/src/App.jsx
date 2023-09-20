f5674fbcd41390e5f47b54b9ad6ec657dd27fbff
Muhammad Salimy Ahsan <71083634+ms-ahsan@users.noreply.github.com>
2023-09-20 23:45:10 +0700

    worldwise : video 219

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -1,5 +1,10 @@
 import { useEffect, useState } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import {
+  BrowserRouter,
+  Navigate,
+  Route,
+  Routes,
+} from 'react-router-dom';
 import Homepage from './pages/Homepage';
 import Product from './pages/Product';
 import Pricing from './pages/Pricing';
@@ -41,12 +46,7 @@ export default function App() {
         <Route path='pricing' element={<Pricing />} />
         <Route path='login' element={<Login />} />
         <Route path='app' element={<AppLayout />}>
-          <Route
-            index
-            element={
-              <CityList cities={cities} isLoading={isLoading} />
-            }
-          />
+          <Route index element={<Navigate to='cities' replace />} />
           <Route
             path='cities'
             element={
