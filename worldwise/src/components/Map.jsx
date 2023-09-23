d145f96ab1cdf4f7290c6a6ad9fcf94f5207981c
Muhammad Salimy Ahsan <71083634+ms-ahsan@users.noreply.github.com>
2023-09-23 20:07:44 +0700

    worldwise : video 232

--- worldwise/src/components/Map.jsx
+++ worldwise/src/components/Map.jsx
@@ -1,23 +1,29 @@
+/* eslint-disable react/prop-types */
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import {
   MapContainer,
   TileLayer,
   Marker,
   Popup,
+  useMap,
+  useMapEvents,
 } from 'react-leaflet';
 import styles from './Map.module.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useCities } from '../contexts/CitiesContext';
 
 export default function Map() {
-  const navigate = useNavigate();
   const { cities } = useCities();
 
   const [mapPosition, setMapPosition] = useState([40, 0]);
 
   const [searchParams, setSearchParam] = useSearchParams();
-  const lat = searchParams.get('lat');
-  const lng = searchParams.get('lng');
+  const mapLat = searchParams.get('lat');
+  const mapLng = searchParams.get('lng');
+
+  useEffect(() => {
+    if (mapLat && mapLng) setMapPosition([mapLat, mapLng]);
+  }, [mapLat, mapLng]);
 
   return (
     <div
@@ -46,7 +52,25 @@ export default function Map() {
             </Popup>
           </Marker>
         ))}
+        <ChangeCenter position={mapPosition} />
+        <DetectClick />
       </MapContainer>
     </div>
   );
+}
+
+function ChangeCenter({ position }) {
+  const map = useMap();
+  map.setView(position);
+
+  return null;
+}
+
+function DetectClick() {
+  const navigate = useNavigate();
+
+  useMapEvents({
+    click: (e) =>
+      navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lat}`),
+  });
 }
