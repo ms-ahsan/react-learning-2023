475bd9c038604a792e034d61543f64037ada3432
Muhammad Salimy Ahsan <71083634+ms-ahsan@users.noreply.github.com>
2023-09-25 02:57:10 +0700

    worldwise : video 240

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -16,6 +16,7 @@ import Form from './components/Form';
 import CountryList from './components/CountryList';
 import { CitiesProvider } from './contexts/CitiesContext';
 import { AuthProvider } from './contexts/FakeAuthContext';
+import ProtectedRoute from './pages/ProtectedRoute';
 
 export default function App() {
   return (
@@ -27,7 +28,14 @@ export default function App() {
             <Route path='product' element={<Product />} />
             <Route path='pricing' element={<Pricing />} />
             <Route path='login' element={<Login />} />
-            <Route path='app' element={<AppLayout />}>
+            <Route
+              path='app'
+              element={
+                <ProtectedRoute>
+                  <AppLayout />
+                </ProtectedRoute>
+              }
+            >
               <Route
                 index
                 element={<Navigate replace to='cities' />}
