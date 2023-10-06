21843e7eb1e7f4be9675facc99045197b96b0e11
Muhammad Salimy Ahsan <71083634+ms-ahsan@users.noreply.github.com>
2023-10-06 21:37:03 +0700

    redux-intro : video 269

--- redux-intro/src/features/accounts/BalanceDisplay.js
+++ redux-intro/src/features/accounts/BalanceDisplay.js
@@ -1,12 +1,19 @@
+import { connect } from 'react-redux';
+
 function formatCurrency(value) {
-  return new Intl.NumberFormat("en", {
-    style: "currency",
-    currency: "USD",
+  return new Intl.NumberFormat('en', {
+    style: 'currency',
+    currency: 'USD',
   }).format(value);
 }
 
-function BalanceDisplay() {
-  return <div className="balance">{formatCurrency(123456)}</div>;
+function BalanceDisplay({ balance }) {
+  return <div className='balance'>{formatCurrency(balance)}</div>;
+}
+function mapStateToProps(state) {
+  return {
+    balance: state.account.balance,
+  };
 }
 
-export default BalanceDisplay;
+export default connect(mapStateToProps)(BalanceDisplay);
