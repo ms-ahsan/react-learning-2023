0c7f98dfefe4a0738c17596a0f31fc18b22bea87
Muhammad Salimy Ahsan <71083634+ms-ahsan@users.noreply.github.com>
2023-10-06 20:26:15 +0700

    redux-intro : video 267

--- redux-intro/src/App.js
+++ redux-intro/src/App.js
@@ -2,7 +2,6 @@ import CreateCustomer from './features/customers/CreateCustomer';
 import Customer from './features/customers/Customer';
 import AccountOperations from './features/accounts/AccountOperations';
 import BalanceDisplay from './features/accounts/BalanceDisplay';
-import store from './store';
 
 function App() {
   return (
