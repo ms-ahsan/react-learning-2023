02367cff30da6f0f183bf84b7661ad73a25a1a4f
Muhammad Salimy Ahsan <71083634+ms-ahsan@users.noreply.github.com>
2023-10-27 10:30:48 +0700

    fast-react-pizza : video 291

--- fast-react-pizza/src/App.jsx
+++ fast-react-pizza/src/App.jsx
@@ -9,7 +9,9 @@ import Error from './ui/Error';
 import Menu, { loader as menuLoader } from './features/menu/Menu';
 import Cart from './features/cart/Cart';
 import Order, { loader as orderLoader } from './features/order/Order';
-import CreateOrder from './features/order/CreateOrder';
+import CreateOrder, {
+  action as createOrderAction,
+} from './features/order/CreateOrder';
 
 const router = createBrowserRouter([
   {
@@ -33,6 +35,7 @@ const router = createBrowserRouter([
       {
         path: '/order/new',
         element: <CreateOrder />,
+        action: createOrderAction,
       },
       {
         path: '/order/:orderId',
