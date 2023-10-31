25cbfdc3c84c9853c5da4e7cd1e4b41cd6bcbfe1
Muhammad Salimy Ahsan <71083634+ms-ahsan@users.noreply.github.com>
2023-10-31 11:31:03 +0700

    fast-react-pizza : video 320

---
+++ fast-react-pizza/src/features/cart/UpdateItemQuantity.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable react/prop-types */
+import { useDispatch } from "react-redux";
+import Button from "../../ui/Button";
+import { decreaseItemQuantity, increaseItemQuantity } from "./cartSlice";
+
+export default function UpdateItemQuantity({pizzaId, currentQuantity}) {
+
+  const dispatch = useDispatch()
+  
+  return (
+    <div className="flex gap-2 items-center md:gap-3">
+      <Button type='round'onClick={()=>dispatch(decreaseItemQuantity(pizzaId))}>-</Button>
+      <span className="text-sm font-md">{currentQuantity}</span>
+      <Button type='round'onClick={()=>dispatch(increaseItemQuantity(pizzaId))}>+</Button>
+    </div>
+  )
+}
