85caeec383046dd6292ff232a2e26c2fb7aeeaf9
Muhammad Salimy Ahsan <71083634+ms-ahsan@users.noreply.github.com>
2023-10-31 11:08:23 +0700

    fast-react-pizza : video 319

--- fast-react-pizza/src/features/menu/MenuItem.jsx
+++ fast-react-pizza/src/features/menu/MenuItem.jsx
@@ -1,13 +1,19 @@
 /* eslint-disable react/prop-types */
-import {useDispatch} from 'react-redux'
+import {useDispatch, useSelector} from 'react-redux'
 import Button from '../../ui/Button';
 import { formatCurrency } from '../../utils/helpers';
-import { addItem } from '../cart/cartSlice';
+import { addItem, getCurrentQuantityById } from '../cart/cartSlice';
+import DeleteItem from '../cart/DeleteItem';
 
 function MenuItem({ pizza }) {
   const dispatch = useDispatch()
 
+  
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+  
+  const currentQuantity = useSelector(getCurrentQuantityById(id))
+
+  const isInCart = currentQuantity > 0
 
   function handleAddToCart(){
     const newItem = {
@@ -41,7 +47,9 @@ function MenuItem({ pizza }) {
             </p>
           )}
 
-         {!soldOut && <Button type="small" onClick={handleAddToCart}>Add to cart</Button>}
+        {isInCart && <DeleteItem pizzaId={id}/> }
+
+         {!soldOut && !isInCart && <Button type="small" onClick={handleAddToCart}>Add to cart</Button>}
         </div>
       </div>
     </li>
