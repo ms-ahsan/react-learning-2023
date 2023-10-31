bdf040d6d3dfaa2adf2278bed1bbd547b590db7f
Muhammad Salimy Ahsan <71083634+ms-ahsan@users.noreply.github.com>
2023-11-01 00:16:40 +0700

    fast-react-pizza : video 324

--- fast-react-pizza/src/features/order/OrderItem.jsx
+++ fast-react-pizza/src/features/order/OrderItem.jsx
@@ -1,8 +1,10 @@
+/* eslint-disable react/prop-types */
 import { formatCurrency } from '../../utils/helpers';
 
 function OrderItem({ item, isLoadingIngredients, ingredients }) {
   const { quantity, name, totalPrice } = item;
 
+  console.log(ingredients);
   return (
     <li className="py-3">
       <div className="flex items-center justify-between gap-4 text-sm">
@@ -10,6 +12,7 @@ function OrderItem({ item, isLoadingIngredients, ingredients }) {
           <span className="font-bold">{quantity}&times;</span> {name}
         </p>
         <p className="font-bold">{formatCurrency(totalPrice)}</p>
+        <p className='text-sm capitalize italic text-stone-500'>{isLoadingIngredients ? 'loading...' : ingredients.join(', ')}</p>
       </div>
     </li>
   );
