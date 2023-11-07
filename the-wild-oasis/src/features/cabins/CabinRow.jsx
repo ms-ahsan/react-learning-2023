d5e05eb0c0ac2f2fb30c02ed1c9141170857884c
MS Ahsan <ahsanaan1306@gmail.com>
2023-11-08 00:19:48 +0700

    the-wild-oasis : video 351

--- the-wild-oasis/src/features/cabins/CabinRow.jsx
+++ the-wild-oasis/src/features/cabins/CabinRow.jsx
@@ -4,6 +4,7 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { formatCurrency } from '../../utils/helpers';
 import styled from 'styled-components';
 import { deleteCabin } from '../../services/apiCabins';
+import toast from 'react-hot-toast';
 
 const TableRow = styled.div`
   display: grid;
@@ -60,12 +61,12 @@ function CabinRow({ cabin }) {
     // mutationFn: (id) => deleteCabin(id),
     mutationFn: deleteCabin,
     onSuccess: () => {
-      alert('Cabin succesfully deleted');
+      toast.success('Cabin succesfully deleted');
       queryClient.invalidateQueries({
         queryKey: ['cabins'],
       });
     },
-    onError: (err) => alert(err.message),
+    onError: (err) => toast.error(err.message),
   });
 
   return (
