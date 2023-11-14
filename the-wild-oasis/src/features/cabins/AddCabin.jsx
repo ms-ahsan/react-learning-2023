4694843a37d3fa9a491581153a5267e3020a804f
MS Ahsan <ahsanaan1306@gmail.com>
2023-11-15 00:03:39 +0700

    the-wild-oasis : vide 368

--- the-wild-oasis/src/features/cabins/AddCabin.jsx
+++ the-wild-oasis/src/features/cabins/AddCabin.jsx
@@ -1,25 +1,49 @@
-import { useState } from 'react';
 import Button from '../../ui/Button';
-import CreateCabinForm from './CreateCabinForm';
 import Modal from '../../ui/Modal';
+import CabinTable from './CabinTable';
+import CreateCabinForm from './CreateCabinForm';
 
 function AddCabin() {
-  const [isOpenModal, setIsOpenModal] = useState(false);
-
   return (
-    <div>
-      <Button onClick={() => setIsOpenModal((show) => !show)}>
-        Add new Cabin
-      </Button>
-      {isOpenModal && (
-        <Modal onClose={() => setIsOpenModal(false)}>
-          <CreateCabinForm
-            onCloseModal={() => setIsOpenModal(false)}
-          />
-        </Modal>
-      )}
-    </div>
+    <>
+      <Modal>
+        <Modal.Open opens='cabin-form'>
+          <Button>Add New cabin</Button>
+        </Modal.Open>
+        <Modal.Window name='cabin-form'>
+          <CreateCabinForm />
+        </Modal.Window>
+      </Modal>
+
+      <Modal>
+        <Modal.Open opens='table'>
+          <Button>Show table</Button>
+        </Modal.Open>
+        <Modal.Window name='table'>
+          <CabinTable />
+        </Modal.Window>
+      </Modal>
+    </>
   );
 }
+
+// function AddCabin() {
+//   const [isOpenModal, setIsOpenModal] = useState(false);
+
+//   return (
+//     <div>
+//       <Button onClick={() => setIsOpenModal((show) => !show)}>
+//         Add new Cabin
+//       </Button>
+//       {isOpenModal && (
+//         <Modal onClose={() => setIsOpenModal(false)}>
+//           <CreateCabinForm
+//             onCloseModal={() => setIsOpenModal(false)}
+//           />
+//         </Modal>
+//       )}
+//     </div>
+//   );
+// }
 
 export default AddCabin;
--- the-wild-oasis/src/ui/Modal-v1.jsx
+++ the-wild-oasis/src/ui/Modal-v1.jsx
@@ -1,10 +1,4 @@
 /* eslint-disable react/prop-types */
-import {
-  cloneElement,
-  createContext,
-  useContext,
-  useState,
-} from 'react';
 import { createPortal } from 'react-dom';
 import { HiOutlineXMark } from 'react-icons/hi2';
 import styled from 'styled-components';
@@ -58,49 +52,18 @@ const Button = styled.button`
   }
 `;
 
-const ModalContext = createContext();
-
-function Modal({ children }) {
-  const [openName, setOpenName] = useState('');
-
-  const close = () => setOpenName('');
-  const open = setOpenName;
-
-  return (
-    <ModalContext.Provider value={{ openName, close, open }}>
-      {children}
-    </ModalContext.Provider>
-  );
-}
-
-function Open({ children, opens: opensWindowName }) {
-  const { open } = useContext(ModalContext);
-
-  // karena tidak bisa secara lagnsung di taruh pada <Button></Button>
-  return cloneElement(children, {
-    onClick: () => open(opensWindowName),
-  });
-}
-
-function Window({ children, name }) {
-  const { openName, close } = useContext(ModalContext);
-
-  if (name !== openName) return null;
-
+function Modal({ children, onClose }) {
   return createPortal(
     <Overlay>
       <StyledModal>
-        <Button onClick={close}>
+        <Button onClick={onClose}>
           <HiOutlineXMark />
         </Button>
-        <div>{cloneElement(children, { onCloseModal: close })}</div>
+        <div>{children}</div>
       </StyledModal>
     </Overlay>,
     document.body
   );
 }
-
-Modal.Open = Open;
-Modal.Window = Window;
 
 export default Modal;
