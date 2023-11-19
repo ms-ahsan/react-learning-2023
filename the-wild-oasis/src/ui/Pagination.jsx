17e30fdb5dbf61e0701734c59dc10d6f625e2609
Muhammad Salimy Ahsan <71083634+ms-ahsan@users.noreply.github.com>
2023-11-19 11:24:43 +0700

    the-wild-oasis : video 382

--- the-wild-oasis/src/ui/Pagination.jsx
+++ the-wild-oasis/src/ui/Pagination.jsx
@@ -5,6 +5,7 @@ import {
 } from 'react-icons/hi2';
 import { useSearchParams } from 'react-router-dom';
 import styled from 'styled-components';
+import { PAGE_SIZE } from '../utils/constants';
 
 const StyledPagination = styled.div`
   width: 100%;
@@ -64,8 +65,6 @@ const PaginationButton = styled.button`
     color: var(--color-brand-50);
   }
 `;
-
-const PAGE_SIZE = 10;
 
 function Pagination({ count }) {
   const [searchParams, setSearchParams] = useSearchParams();
