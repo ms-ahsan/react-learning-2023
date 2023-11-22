c3b4040ea215f753f6cd5c35439445fdb8af5f3c
Muhammad Salimy Ahsan <71083634+ms-ahsan@users.noreply.github.com>
2023-11-22 23:56:34 +0700

    the-wild-oasis : video 400

--- the-wild-oasis/src/features/dashboard/DashboardLayout.jsx
+++ the-wild-oasis/src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,8 @@
+/* eslint-disable react/no-unescaped-entities */
 import styled from 'styled-components';
+import { useRecentBookings } from './useRecentBookings';
+import Spinner from '../../ui/Spinner';
+import { useRecentStays } from './useRecentStays';
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -8,6 +12,16 @@ const StyledDashboardLayout = styled.div`
 `;
 
 function DashboardLayout() {
+  const { bookings, isLoading } = useRecentBookings();
+  const {
+    stays,
+    confirmedSttays,
+    isLoading: isLoadingStays,
+  } = useRecentStays();
+
+  if (isLoading || isLoadingStays) return <Spinner />;
+
+  console.log(bookings);
   return (
     <StyledDashboardLayout>
       <div>Statistics</div>
